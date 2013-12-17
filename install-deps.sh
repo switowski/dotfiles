f6825b4fa44db58da813be2a260003102a5681ab
switowski <witowski.sebastian@gmail.com>
2013-12-17 21:44:04 +0100

    Added new files
    * Adds .aliases, .bash_profile, .bash_prompt (need to test this one), install-deps.sh
    * Adds more aliases to .gitconfig

---
+++ install-deps.sh
@@ -0,0 +1,10 @@
+# up to you if you want to run this as a file or copy paste at your leisure
+
+# https://github.com/rupa/z
+# z, oh how i love you
+cd
+git clone https://github.com/rupa/z.git
+chmod +x ~/z/z.sh
+# also consider moving over your current .z file if possible. it's painful to rebuild :)
+
+# z binary is already referenced from .bash_profile
