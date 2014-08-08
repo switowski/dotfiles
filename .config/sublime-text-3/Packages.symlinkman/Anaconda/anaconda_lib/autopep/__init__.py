596faefc364b3a7bc22432b8fce3f02f69c5d413
Sebastian Witowski <sebastian.witowski@cern.ch>
2014-08-08 17:20:23 +0200

    Removing installed sublime packages

--- .config/sublime-text-3/Packages.symlinkman/Anaconda/anaconda_lib/autopep/__init__.py
+++
@@ -1,8 +0,0 @@
-
-# Copyright (C) 2013 - Oscar Campos <oscar.campos@member.fsf.org>
-# This program is Free Software see LICENSE file for details
-
-from .autopep_wrapper import AnacondaAutopep8
-
-
-__all__ = ['AnacondaAutopep8']
--- .config/sublime-text-3/Packages.symlinkman/Anaconda/anaconda_server/__init__.py
+++
@@ -1,3 +0,0 @@
-
-# Copyright (C) 2013 - Oscar Campos <oscar.campos@member.fsf.org>
-# This program is Free Software see LICENSE file for details
